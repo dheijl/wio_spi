c5f310780166796a9e3c91c16b90f1a2246a58c8
dheijl <danny.heijl@telenet.be>
2024-12-12 15:12:31 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,7 @@ const auto spi_sck = PIN_SPI_SCK;   // (49ul) = BCM11 = pin 23 on wio breakout
 // local function declarations here:
 static void setup_debug();
 static void setup_spi();
+static void print_hex(uint8_t x);
 
 void setup()
 {
@@ -34,7 +35,47 @@ void setup()
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
+
+  unsigned char txbuf[64];
+  memset(txbuf, 0xff, sizeof(txbuf));
+  unsigned char rxbuf[64];
+  memset(rxbuf, 0xff, sizeof(rxbuf));
+  txbuf[0] = '?';
+  txbuf[1] = 0;
+  size_t count = 2;
+
+  tft_println("Start SPI");
+  digitalWrite(PIN_SPI_SS, LOW);
+  SPI.beginTransaction(SPISettings((int)12000000, MSBFIRST, (uint8_t)SPI_MODE0));
+
+  // TXBUF != NULL => write and read simultaneously
+  // TXBUF == NULL => read only
+  SPI.transfer(txbuf, NULL, count, false);
+  SPI.waitForTransfer();
+  SPI.endTransaction();
+  digitalWrite(PIN_SPI_SS, HIGH);
+  tft_println("SPI complete");
+#ifdef DEBUG
+  for (size_t i = 0; i < sizeof(rxbuf); i++)
+  {
+    print_hex(rxbuf[i]);
+  }
+  Serial.println();
+  for (size_t i = 0; i < sizeof(txbuf); i++)
+  {
+    print_hex(txbuf[i]);
+  }
+  Serial.println();
+#endif
+}
+
+static void print_hex(uint8_t x)
+{
+  if (x < 16)
+  {
+    Serial.print('0');
+  }
+  Serial.print(x, HEX);
 }
 
 static void setup_debug()
@@ -54,9 +95,11 @@ static void setup_spi()
 {
   tft_println("SPI setup");
   SPI.begin();
+  /*
   SPI.setBitOrder(MSBFIRST);
   SPI.setDataMode(SPI_MODE0);
   SPI.setClockDivider(1);
+  */
   tft_println("SPI initialised");
   delay(500);
   while (digitalRead(WIO_KEY_B) == HIGH)
